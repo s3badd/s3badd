b132b48b87b6fc94fa4e552026dfd00daefb8f3d
s3badd <153804431+s3badd@users.noreply.github.com>
2024-03-17 06:53:48 -0400

    Update spark-submit-k8-ex.sh

--- spark-submit-k8-ex.sh
+++ spark-submit-k8-ex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 spark-submit \
-    --master k8s://https://kubernetes.default.svc.cluster.local:16443 \
+    --master k8s://https://0.0.0.0.default.svc.cluster.local:16443 \
     --deploy-mode cluster \
     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
     --conf spark.kubernetes.authenticate.caCertFile=/var/snap/microk8s/current/certs/ca.crt \
