d642dc792a2f48637be3fc445da52c6a8dbe7bb3
s3badd <153804431+s3badd@users.noreply.github.com>
2024-03-17 06:51:26 -0400

    Create spark-submit-k8-ex.sh

---
+++ spark-submit-k8-ex.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+spark-submit \
+    --master k8s://https://kubernetes.default.svc.cluster.local:16443 \
+    --deploy-mode cluster \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+    --conf spark.kubernetes.authenticate.caCertFile=/var/snap/microk8s/current/certs/ca.crt \
+    --conf spark.kubernetes.authenticate.submission.oauthToken=$K8S_TOKEN \
+    --conf spark.kubernetes.container.image=spark-rapids \
